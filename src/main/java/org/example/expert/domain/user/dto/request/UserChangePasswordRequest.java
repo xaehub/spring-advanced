55d7244c2e39c323727259c3bc93043fc37df97f
xaehub <novxae@gmail.com>
2025-04-16 18:05:55 +0900

    레벨1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 8글자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에 숫자를 포함시켜 주세요.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에 대문자를 포함시켜 주세요.")
     private String newPassword;
 }
