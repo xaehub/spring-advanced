b5edeab47adcb6c5941d7e39a13ce1e992b8007e
xaehub <novxae@gmail.com>
2025-04-18 15:34:53 +0900

    레벨 5: 코드 리팩토링 - 비밀번호 수정할 때만 있던 비밀번호 유효성 검사를 회원가입할 때도 할 수 있게 수정

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ public class SignupRequest {
     @NotBlank @Email
     private String email;
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 8글자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에 숫자를 포함시켜 주세요.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에 대문자를 포함시켜 주세요.")
     private String password;
     @NotBlank
     private String userRole;
